6e67a0edb11ec7c744b1163b4813d6bb9945b024
Mathew <mathewlmcdowell@gmail.com>
2023-09-14 19:01:34 -0500

    import browserrouter setup

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react';
-import reactLogo from './assets/react.svg';
-import Login from './components/Login';
+import { useState } from 'react'
+import { Routes, Route, Link } from 'react-router-dom'
+import Login from './components/Login'
 
 function App() {
   const [count, setCount] = useState(0);
  
   return (
     <div className='App'>
-        <h1>Boilerplate</h1>
-        <img id='comp-img' src='./computer.png'></img>
-        <p>Replace the starter code in this template with something cool</p>
-        <Login />
+      <h1>Help</h1>
     </div>
   );
 }
