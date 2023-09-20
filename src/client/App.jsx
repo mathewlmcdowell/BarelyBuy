6724a140d728d1ef554167b8b5f52add82ae150d
Mathew <mathewlmcdowell@gmail.com>
2023-09-19 19:44:24 -0500

    faker

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
+import Home from './components/Home'
 
 function App() {
   const [count, setCount] = useState(0);
  
   return (
-    <div className='App'>
-      <h1>Help</h1>
-    </div>
+    <>
+      <div>
+        <Routes>
+          <Route path ='/' element={<Home />} />
+        </Routes>
+      </div>
+    </>
   );
 }
 
