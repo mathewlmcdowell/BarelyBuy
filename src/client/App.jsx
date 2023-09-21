46c48441a90acb03ef6f536f72260264dceefadd
Mathew <mathewlmcdowell@gmail.com>
2023-09-21 17:24:44 -0500

    faker relocation to components

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -2,12 +2,16 @@ import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
+import Faker from './components/Faker'
 
 function App() {
   const [count, setCount] = useState(0);
  
   return (
     <>
+    <div>
+      <Faker />
+    </div>
       <div>
         <Routes>
           <Route path ='/' element={<Home />} />
