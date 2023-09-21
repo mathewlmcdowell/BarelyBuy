c499979e6c03742b5a429c6c42d0842d84b29d6f
Mathew <mathewlmcdowell@gmail.com>
2023-09-21 17:28:20 -0500

    syntax

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,11 +1,10 @@
-
 import { useState } from 'react'
 import { Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Faker from './components/Faker'
 
-
+export default function App(){
   return (
     <>
     <div>
