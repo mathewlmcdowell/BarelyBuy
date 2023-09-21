6fe71fde20212ec38c692af7b825832affba30ff
Mathew <mathewlmcdowell@gmail.com>
2023-09-21 18:00:38 -0500

    working faker relocation

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -3,6 +3,7 @@ import { Routes, Route, Link } from 'react-router-dom'
 import Login from './components/Login'
 import Home from './components/Home'
 import Faker from './components/Faker'
+import Card from './components/Card'
 
 function App() {
  
