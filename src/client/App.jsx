723a627230a741b9f29ed416d9bc9fe30a8a1423
Mathew <mathewlmcdowell@gmail.com>
2023-09-28 21:12:41 -0500

    relocated home to be a parent because i have a headache, all functionality is the same

--- src/client/App.jsx
+++ src/client/App.jsx
@@ -1,21 +1,24 @@
 import React, { useState, useEffect } from "react";
 import { Routes, Route, Link } from "react-router-dom";
-import Home from "./components/Home";
+import Home from "./Home";
 import './style.css'
 import Login from "./components/Login";
 import Register from "./components/Register";
 import Cart from './components/Cart';
 import ProductDetail from "./components/ProductDetail";
 import SearchBar from "./components/SearchBar";
+import Faker from "./components/Faker";
 
 
-export default function App({ onSearch }) {
+export default function App({ onSearch, product, cartItems}) {
+
+
 
   return(
     <>
     <Routes>
       <Route path="/" element={<Home />} />
-      <Route path="/cart" element={<Cart />} />
+      <Route path="/Cart" element={<Cart />} />
       <Route path="/product/:id" element={<ProductDetail />} />
       <Route path ='/Login' element={<Login />} />
       <Route path ='/Register' element={<Register />} />
--- src/client/components/NavBar.jsx
+++ src/client/components/NavBar.jsx
@@ -1,5 +1,6 @@
 import { Link } from "react-router-dom"
 
+
 export default function NavBar() {
 
 
@@ -7,13 +8,13 @@ export default function NavBar() {
         <>
         <div className="NavBar outline">
             <div>
-
-            </div>
-            <div>
                 <Link to = '/Login' className='navButton'>Login</Link>
                 <Link to = '/Register' className="navButton">Register</Link>
             </div>
             <Link to = '/' className="title">barely buy</Link>
+            <div>
+                <Link to = '/Cart' className="navButton">Cart</Link>
+            </div>
         </div>
         </>
     )
