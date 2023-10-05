26fd17f439dbe57233a1fcb8cc47bcc2624347c4
TabriaJenae Williams <tabriawilliams@gmail.com>
2023-10-05 23:33:41 +0000

    checkout completed/ functionality for login and register

--- src/client/components/Login.jsx
+++ src/client/components/Login.jsx
@@ -1,82 +1,93 @@
-import React, { useState } from 'react';
-import NavBar from './NavBar';
+import { useContext, useState } from "react";
+import { useNavigate } from 'react-router-dom';
+import { API_URL } from "../config";
+import { AppContext } from "../appcontextprovider";
+import Navbar from "./Navbar";
 
 const Login = () => {
-  const [email, setEmail] = useState('');
-  const [password, setPassword] = useState('');
-  const [message, setMessage] = useState('');
+    const { setIslogin, setUser } = useContext(AppContext);
 
-  const handleEmailChange = (e) => {
-    setEmail(e.target.value);
-  };
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
-  const handlePasswordChange = (e) => {
-    setPassword(e.target.value);
-  };
+    const navigate = useNavigate();
 
-  const login = async() => {
-    try {
-        const response = await fetch('http://localhost:3000/api/users/login', {
+
+    const handleLogin = async (e) => {
+        e.preventDefault();
+
+        const data = JSON.stringify({
+            email: email,
+            password: password,
+        })
+
+        const h = new Headers();
+        h.append('content-type', 'application/json');
+
+        setIsLoading(true);
+
+        const response = await fetch(API_URL + '/login', {
+            headers: h,
             method: 'POST',
-            headers: {
-                'Content-Type' : 'application/json'
-            }, 
-            body: JSON.stringify({
-                email,
-                password
-            })
-        });
-        const result = await response.json();
-        setMessage(result.message);
-        if(!response.ok) {
-          throw(result)
+            body: data,
+        })
+
+        const responseData = await response.json();
+
+        setIsLoading(false);
+
+        if (responseData.error) {
+            alert(responseData.errorMessage);
+        } else {
+            setEmail('');
+            setPassword('');
+
+            const token = responseData.token;
+            sessionStorage.setItem('shoppingToken', token);
+
+            setIslogin(true);
+            setUser(responseData.user);
+
+            alert('Login successfull');
+            navigate('/dashboard');
         }
-        setEmail('');
-        setPassword('');
-    } catch (err) {
-        console.error(`${err.name}: ${err.message}`);
+
+
     }
-  }
 
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    login();
-  };
+    return (
+        <>
+            <Navbar />
+            <div className="container my-5">
+                <div className="row justify-content-center align-items-center" style={{ height: '90vh' }}>
+                    <div className="col-6">
+                        <div className="card card-primary">
+                            <div className="card-header">
+                                <h2 className="text-center">Login</h2>
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={handleLogin}>
+                                    <div className="form-group">
+                                        <label htmlFor="">Email</label>
+                                        <input type="email" value={email} className="form-control" onChange={e => setEmail(e.currentTarget.value)} required />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="">Password</label>
+                                        <input type="password" value={password} className="form-control" onChange={e => setPassword(e.currentTarget.value)} required />
+                                    </div>
+                                    <div className="form-group my-4">
+                                        <button type="submit" className="btn btn-primary" disabled={isLoading} >{isLoading ? 'loggin in...' : 'LOGIN'}</button>
+                                    </div>
 
-  return (
-    <>
-    <div>
-      <NavBar />
-    </div>
-    <div className='outline padding'>
-      <h2>Login</h2>
-      <form onSubmit={handleSubmit}>
-        <div>
-          <label htmlFor='email'>Email:</label>
-          <input
-            type='email'
-            id='email'
-            value={email}
-            onChange={handleEmailChange}
-            required
-          />
-        </div>
-        <div>
-          <label htmlFor='password'>Password:</label>
-          <input
-            type='password'
-            id='password'
-            value={password}
-            onChange={handlePasswordChange}
-            required
-          />
-        </div>
-        <button type='submit'>Login</button>
-      </form>
-      <p>{message}</p>
-    </div>
-    </>
-  );
-};
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
+}
 
-export default Login;
+export default Login
--- src/client/components/Register.jsx
+++ src/client/components/Register.jsx
@@ -1,110 +1,91 @@
-import React, {useState} from "react";
+import { API_URL } from "../config";
+import { useState } from "react";
 import { useNavigate } from "react-router-dom";
-import NavBar from "./NavBar";
+import Navbar from "./Navbar";
 
 
-export default function Register () {
-
-    const [email, setemail] = useState("");
-    const [password, setpassword] = useState("");
-
-    const [emailerror, setemailerror] = useState(null);
-    const [passworderror, setpassworderror] = useState(null);
+const Register = () => {
+    const [name, setName] = useState('');
+    const [email, setEmail] = useState('');
+    const [password, setPassword] = useState('');
+    const [isLoading, setIsLoading] = useState(false);
 
     const navigate = useNavigate();
-    
 
-    const registerUser = async (event) => {
-        event.preventDefault();
+    const handleRegister = async (e) => {
+        e.preventDefault();
 
-        if (email.length < 6) {
-            setemailerror("Username must be at least 6 characters in length");
-            return;
-          } else {
-            setemailerror(null);
-          }
-          
-          // form validation: password
-          if (password.length < 8) {
-            setpassworderror("Password must be at least 8 characters in length");
-            return;
-          } else {
-            setpassworderror(null);
-          }
+        const data = JSON.stringify({
+            name: name,
+            email: email,
+            password: password,
+        })
 
-    try {
-        const response = await fetch(
-            'http://localhost:3000/api/users/register', {
-          method: "POST",
-          headers: {
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({
-            user: {
-              email: email,
-              password: password
-            }
-          })})
-        ;
-        const result = await response.json();
-        console.log(result)
+        const h = new Headers();
+        h.append('content-type', 'application/json');
+
+        setIsLoading(true);
+
+        const response = await fetch(API_URL + '/register', {
+            headers: h,
+            method: 'POST',
+            body: data,
+        })
+
+        const responseData = await response.json();
+
+        setIsLoading(false);
+
+        if (responseData.error) {
+            alert(responseData.errorMessage);
+        } else {
+            setName('');
+            setEmail('');
+            setPassword('');
+            alert('registeration successfull');
             navigate('/login');
-    
-        return result
-      } catch (err) {
-        console.error(err);
-        
-      }    
-};
 
-return(
-    <>
-    <div>
-      <NavBar />
-    </div>
-    <div className="padding">
-<h2>Don't have an account? Sign Up</h2>
- <form
- method="POST"
- onSubmit={registerUser} 
-        onClick={() => {
-            setemail(email)
-        }}>
-<label>
-    Create Username:{""}
-    <input 
-        placeholder='Email Address'
-        value = {email}
-        
-         onChange={(e)=> 
-        setemail(e.target.value)} />
-</label>
-{emailerror && <p style={{ color: "red"}}>{emailerror}</p>} 
+        }
 
-<label >
-        Create Password:{""}
-         <input 
-         placeholder='Create Password'
-         type='password'
-         value={password}
-         onChange={(e)=> 
-         setpassword(e.target.value)}/>
-      </label>
-    
-         {passworderror && <p style={{ color: "red"}}>{passworderror}</p>}
 
-         <button  type='Submit' style=
-            {{width: "80px", height: "37px", padding: "10px", 
-            fontSize:"15px"}}
-           
-            //onClick={() => {
-             //   navigate('/login');
-            //}}
-            >Register</button>
-        
- </form>
- </div>
-         </>
- );
+    }
+
+    return (
+        <>
+            <Navbar />
+            <div className="container my-5">
+                <div className="row justify-content-center align-items-center" style={{ height: '90vh' }}>
+                    <div className="col-6">
+                        <div className="card card-primary">
+                            <div className="card-header">
+                                <h2 className="text-center">Register</h2>
+                            </div>
+                            <div className="card-body">
+                                <form onSubmit={handleRegister}>
+                                    <div className="form-group">
+                                        <label htmlFor="">Name</label>
+                                        <input type="text" value={name} className="form-control" onChange={e => setName(e.currentTarget.value)} required />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="">Email</label>
+                                        <input type="email" value={email} className="form-control" onChange={e => setEmail(e.currentTarget.value)} required />
+                                    </div>
+                                    <div className="form-group">
+                                        <label htmlFor="">Password</label>
+                                        <input type="password" value={password} className="form-control" onChange={e => setPassword(e.currentTarget.value)} required />
+                                    </div>
+                                    <div className="form-group my-4">
+                                        <button type="submit" className="btn btn-primary" disabled={isLoading} >{isLoading ? 'Registering...' : 'REGISTER'}</button>
+                                    </div>
+
+                                </form>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </>
+    )
 }
-Register;
+
+export default Register
